4c71bb5346fda78590ce7ba2fd4775c1536e83e4
Quirk0o <beata@obrok.eu>
2016-01-03 07:30:10 +0100

    Add siplification of tuples

--- simplifier_combinator/src/main/scala/simplifier/Simplifier.scala
+++ simplifier_combinator/src/main/scala/simplifier/Simplifier.scala
@@ -9,7 +9,20 @@ object Simplifier {
 
   def simplify(node: Node): Node = node match {
     case NodeList(list) => NodeList(list.map(simplify))
+    case e @ BinExpr(_, _, _) => simplifyBinExpr(e)
     case _ => node
   }
+
+  def simplifyBinExpr(binExpr: Node): Node = binExpr match {
+    case BinExpr("+", t1 @ Tuple(_), t2 @ Tuple(_)) => concatTuples(t1, t2)
+    case BinExpr(op, left, right) =>
+      val e = BinExpr(op, simplify(left), simplify(right))
+      if (e.left != left || e.right != right)
+        simplifyBinExpr(e)
+      else
+        e
+  }
+
+  def concatTuples(t1: Tuple, t2: Tuple) = Tuple(t1.list ++ t2.list)
 
 }
